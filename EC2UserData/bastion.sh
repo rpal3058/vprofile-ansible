e9eea69b3ba8278c2dec4da4416646c140442670
rpal3058 <81841030+rpal3058@users.noreply.github.com>
2022-05-20 06:36:36 +0530

    update bastion host_v16

--- EC2UserData/bastion.sh
+++ EC2UserData/bastion.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo yum update -y
 sudo amazon-linux-extras install ansible2
+sudo yum install git -y
+sudo yum update -y
 git init
 git clone https://github.com/rpal3058/vprofile-ansible.git
 cd vprofile-ansible
